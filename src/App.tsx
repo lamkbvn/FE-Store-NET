68c9b7e2edfe5e501a7dff7c6f0fbfcd31abec28
[dyad] <git@dyad.sh>
2025-10-07 23:02:28 +0700

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import StorePage from "./pages/StorePage";
 import ProductDetailPage from "./pages/ProductDetailPage";
-import CashierPage from "./pages/CashierPage"; // Import CashierPage
+import CashierPage from "./pages/CashierPage";
 import Header from "./components/Header";
 
 const queryClient = new QueryClient();
@@ -18,10 +17,10 @@ const App = () => (
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<CashierPage />} /> {/* CashierPage is now the default route */}
           <Route path="/store" element={<StorePage />} />
           <Route path="/products/:id" element={<ProductDetailPage />} />
-          <Route path="/cashier" element={<CashierPage />} /> {/* New route for Cashier Page */}
+          <Route path="/cashier" element={<CashierPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
