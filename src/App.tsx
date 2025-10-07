06e7b412183a18d09f3f9047d7dc042863a636b6
[dyad] <git@dyad.sh>
2025-10-07 22:26:16 +0700

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster as Sonner } from "@/components/ui/sonner"; // Keep sonner's Toaster
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import StorePage from "./pages/StorePage";
-import Header from "./components/Header"; // Import the new Header component
+import Header from "./components/Header";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
+      {/* Removed shadcn/ui Toaster to use sonner consistently */}
+      <Sonner /> {/* Render sonner's Toaster */}
       <BrowserRouter>
-        <Header /> {/* Render the Header component here */}
+        <Header />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/store" element={<StorePage />} />
